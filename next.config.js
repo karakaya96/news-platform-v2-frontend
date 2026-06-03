576c642090586dcf492c05a3f9b9bcaa7b9f1dfe
Karakaya <karakaya.mk96@gmail.com>
2026-06-03 20:10:00 +0300

    fix: cache-control headerlari categories ve news sayfalarina eklendi

--- next.config.js
+++ next.config.js
@@ -11,6 +11,24 @@ const nextConfig = {
           { key: 'Vercel-CDN-Cache-Control', value: 'no-store' },
         ],
       },
+      {
+        source: '/categories/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, max-age=0' },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'CDN-Cache-Control', value: 'no-store' },
+          { key: 'Vercel-CDN-Cache-Control', value: 'no-store' },
+        ],
+      },
+      {
+        source: '/news/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, max-age=0' },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'CDN-Cache-Control', value: 'no-store' },
+          { key: 'Vercel-CDN-Cache-Control', value: 'no-store' },
+        ],
+      },
     ];
   },
 };
